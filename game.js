f7fb1842247005a47a068b607cd9068d2cec2a83
sora-k46 <sorrawit456789@gmail.com>
2022-07-16 14:16:16 +0700

    Create play()

--- game.js
+++ game.js
@@ -7,3 +7,34 @@ function getComputerChoice() {
   ];
   return weapon[Math.floor(Math.random() * 10) % weapon.length];
 }
+
+function play(playerSelection, computerSelection) {
+
+  playerSelection = playerSelection.toLowerCase()
+
+  // Draw
+  if (playerSelection === computerSelection) {
+    return "Draw"
+
+    // Paper
+  } else if (playerSelection === "paper" && computerSelection === "rock") {
+    return "Win"
+  } else if (playerSelection === "paper" && computerSelection === "scissors") {
+    return "Lose"
+
+    // Rock
+  } else if (playerSelection === "rock" && computerSelection === "scissors") {
+    return "Win"
+  }
+  else if (playerSelection === "rock" && computerSelection === "paper") {
+    return "Lose"
+  }
+
+  // Scissors
+  else if (playerSelection === "scissors" && computerSelection === "paper") {
+    return "Win"
+  } else {
+    return "Lose"
+  }
+
+}
